466978c8f372ed4f979ea900b1bdc2302c0b3128
Mr.Fumpledump <Fumpledump@gmail.com>
2022-03-23 20:20:45 -0400

    stuff

--- Good Luck/Game1.cs
+++ Good Luck/Game1.cs
@@ -158,7 +158,7 @@ namespace Good_Luck
             bulletTexture = Content.Load<Texture2D>("Bullet");
 
             // Entity Loading
-            player = new Player(playerRect, playerTexture, 5, 10, 0, 2);
+            player = new Player(playerRect, playerTexture, 5, 10, 0, 8);
 
         }
 
