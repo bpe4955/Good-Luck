9ea0983aa9dd254209090daea4efa256af4ded8b
Brian Egan <bpe4955@rit.edu>
2022-03-25 16:06:47 -0400

    Commented Wall class

--- Good Luck/Wall.cs
+++ Good Luck/Wall.cs
@@ -10,17 +10,28 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Good_Luck
 {
-    class Wall
+    class Wall 
     {
         //fields
         private Rectangle rect;
         private Texture2D texture;
 
         //Properties
+        /// <summary>
+        /// Get and set the wall's rectangle
+        /// </summary>
         public Rectangle Rect { get => rect; set => rect = value; }
+        /// <summary>
+        /// Get and set the wall's texture
+        /// </summary>
         public Texture2D Texture { get => texture; set => texture = value; }
 
         //Constructor
+        /// <summary>
+        /// Constructor for a wall with a rectangle and texture
+        /// </summary>
+        /// <param name="rect">The position and size of the wall</param>
+        /// <param name="texture">The texture of the wall</param>
         public Wall(Rectangle rect, Texture2D texture)
         {
             this.rect = rect;
