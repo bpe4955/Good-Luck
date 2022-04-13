439af74d39803d9364ad19519d74c311201e6edc
Mr.Fumpledump <Fumpledump@gmail.com>
2022-04-13 19:19:41 -0400

    Revert to Ready Program

--- good luck/Game1.cs
+++ good luck/Game1.cs
@@ -264,13 +264,13 @@ namespace Good_Luck
             roomTestTop = levelManager.LoadRoom(new Room("Content/RoomTop.level", Content, entityManager));
             //levelManager.AddRoom(roomTestTop);
             roomTestRight = levelManager.LoadRoom(new Room("Content/RoomRight.level", Content, entityManager));
+            levelManager.LoadRoom(new Room("Content/BlockedOffRightRoom.level", Content, entityManager));
+
             //levelManager.AddRoom(roomTestRight);
             roomTestBottom = levelManager.LoadRoom(new Room("Content/RoomBottom.Level", Content, entityManager));
             //levelManager.AddRoom(roomTestBottom);
             roomTestLeft = levelManager.LoadRoom(new Room("Content/RoomLeft.Level", Content, entityManager));
-            //levelManager.AddRoom(roomTestLeft);
-
-            levelManager.NextLevel();
+            levelManager.LoadRoom(new Room("Content/BlockedOffLeftRoom.Level", Content, entityManager));
 
 
             //Hooking up events
